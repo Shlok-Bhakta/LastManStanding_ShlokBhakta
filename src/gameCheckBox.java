9359e7f994ddef8dfce40dbbf1a5b7c19dec2406
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-05-11 12:01:44 -0500

    UI Is for the most part complete

--- src/gameCheckBox.java
+++ src/gameCheckBox.java
@@ -3,8 +3,16 @@ import javax.swing.*;
 public class gameCheckBox extends JCheckBox {
     JCheckBox checkBox = new JCheckBox();
 
+
+
+
+
+
+
+
     public gameCheckBox(boolean enabled) {
         super();
+        
         this.setEnabled(enabled);
 
     }
