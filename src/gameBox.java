066ef91d15edb4383b538a3934a5bf90a1c89e4d
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-05-12 12:00:37 -0500

    made it possible to only select 3 boxes

--- src/gameBox.java
+++ src/gameBox.java
@@ -9,6 +9,7 @@ public class gameBox extends JFrame implements ActionListener {
 
     GridBagLayout gridBag = new GridBagLayout();
     ArrayList<JCheckBox> boxes = new ArrayList<JCheckBox>();
+    ArrayList<String> boxesID = new ArrayList<String>();
 
     public gameBox() {
         super();
@@ -19,8 +20,7 @@ public class gameBox extends JFrame implements ActionListener {
             this.setTitle("GridBag");
             this.setAlwaysOnTop(true);
             this.setLayout(gridBag);
-            
-            
+
         }
         /* Label */ {
             JLabel label = new JLabel("Select >=3 boxes");
@@ -39,55 +39,17 @@ public class gameBox extends JFrame implements ActionListener {
             this.add(label, c);
 
         }
-        /* checkbox1 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(0, 1));
-            boxes.add(box);
-        }
-        /* checkbox2 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(1, 1, GridBagConstraints.CENTER));
-            boxes.add(box);
-        }
-        /* checkbox3 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(2, 1, GridBagConstraints.CENTER));
-            boxes.add(box);
-        }
-        /* checkbox4 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(3, 1, GridBagConstraints.CENTER));
-            boxes.add(box);
-        }
-        /* checkbox5 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(4, 1, GridBagConstraints.CENTER));
-            boxes.add(box);
-        }
-        /* checkbox6 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(0, 2));
-            boxes.add(box);
-        }
-        /* checkbox7 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(1, 2, GridBagConstraints.CENTER));
-            boxes.add(box);
-        }
-        /* checkbox8 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(2, 2, GridBagConstraints.CENTER));
-            boxes.add(box);
-        }
-        /* checkbox9 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(3, 2, GridBagConstraints.CENTER));
-            boxes.add(box);
-        }
-        /* checkbox10 */ {
-            JCheckBox box = boxMaker();
-            this.add(box, setConstraints(4, 2, GridBagConstraints.CENTER));
-            boxes.add(box);
+        /* add checkboxes */ {
+            int ID = 0;
+            for (int col = 1; col <= 2; col++) {
+                for (int row = 0; row <= 4; row++) {
+
+                    JCheckBox box = boxMaker(ID);
+                    this.add(box, setConstraints(row, col));
+                    boxes.add(box);
+                    ID++;
+                }
+            }
         }
         /* Submit Button */ {
             JButton button = new JButton("Submit Move");
@@ -100,6 +62,7 @@ public class gameBox extends JFrame implements ActionListener {
             } catch (IOException | FontFormatException e) {
 
             }
+            button.addActionListener(this);
             GridBagConstraints c = setConstraints(0, 3, GridBagConstraints.CENTER, GridBagConstraints.CENTER);
             c.gridwidth = 6;
             this.add(button, c);
@@ -109,23 +72,57 @@ public class gameBox extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        //TODO check for only 3 boxes checked
         int boxesChecked = 0;
-        
+        ArrayList<JCheckBox> boxArray = boxes;
+
+        if (e.getActionCommand() == "Submit Move") {
 
 
 
+        } else {
 
+            if (boxesID.contains(e.getActionCommand())) {
+                boxesChecked--;
+                boxesID.remove(e.getActionCommand());
+                System.out.println(boxesID);
 
+            } else {
+                boxesID.add(e.getActionCommand());
+                boxesChecked++;
 
+            }
+            if (boxesID.size() >= 3) {
+                for (int k = 0; k < boxesID.size() - 1; k++) {
+                    for (int l = 0; l < boxes.size(); l++) {
+                        if (!(boxesID.get(k) == boxes.get(l).getActionCommand())) {
+                            boxes.get(l).setEnabled(false);
+
+                        }
+                        if(boxes.get(l).isSelected()){
+                            boxes.get(l).setEnabled(true);
+                        }
+                    }
+                    
+
+                }
+
+            }else{
+                for(int l = 0; l < boxes.size(); l++){
+                    boxes.get(l).setEnabled(true);
+                }
+            }
+        }
     }
 
-    public static JCheckBox boxMaker() {
+    // makes a box with a unique id
+    public JCheckBox boxMaker(int ID) {
         JCheckBox box = new JCheckBox();
-
+        box.addActionListener(this);
+        box.setActionCommand("ID_" + ID);
         return box;
     }
 
+    // 3 overloaded methods for setiing constraints to gridbaglayout
     public static GridBagConstraints setConstraints(int gridx, int gridy, int fill) {
         GridBagConstraints c = new GridBagConstraints();
         c.gridx = gridx;
